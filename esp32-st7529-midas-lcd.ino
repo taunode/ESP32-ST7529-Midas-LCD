6130c966d450ee119322ccfd0bd489d94ecfe693
taunode <40491466+taunode@users.noreply.github.com>
2026-04-04 09:48:18 +0200

    Update and rename sketch_feb1a.ino to esp32-st7529-midas-lcd.ino

--- esp32-st7529-midas-lcd.ino
+++ esp32-st7529-midas-lcd.ino
@@ -1,8 +1,67 @@
+/* ---------------------------------------------------------- */
+// ESP32-Treiber für das ST7529-basierte MCCOG240160C6W-FPTLW //
+// von Tim van den Boom, Studienarbeit im 6. Semester - 2026  //
+/* ---------------------------------------------------------- */
+
+// Wichtige Informationen zum Initialisierungsprozess des Midas-Displays stammen aus folgendem Github-Code:
+// https://github.com/augustofg/libst7529
+
+// Kommentare mit eckigen Klammern beziehen sich als Referenz auf die Seitenzahl im Datenblatt des ST7529
+
+
+/*
+// Erforderliche Libraries ------------------------------------------------------------------------
+*/
+
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Fonts/FreeSansBold7pt7b.h>
 #include <Fonts/FreeSansBold18pt7b.h>
 
+
+/*
+// Globale Variablen ------------------------------------------------------------------------------
+*/
+
+// Puffer: 1 Byte pro Pixel (Werte 0-31)
+// 240 * 160 = 38400 Bytes
+uint8_t framebuffer[38400]; 
+
+
+/*
+// Pin-Definition ---------------------------------------------------------------------------------
+*/
+
+// Für das Schieberegister 595
+const int PIN_SHIFT = 32;   // SRCLK
+const int PIN_STORE = 33;   // RCLK
+const int PIN_DATA  = 25;   // SER
+
+
+// Legacy: Parallelbus
+/*
+const int LCD_D0 = 13; const int LCD_D1 = 12;
+const int LCD_D2 = 33; const int LCD_D3 = 32;
+const int LCD_D4 = 25; const int LCD_D5 = 23;
+const int LCD_D6 = 22; const int LCD_D7 = 21;
+
+*/
+const int LCD_WR = 14;
+const int LCD_RS = 26;
+
+
+/*
+// Legacy: Parallele Pins
+pinMode(LCD_D0, OUTPUT); pinMode(LCD_D1, OUTPUT);
+pinMode(LCD_D2, OUTPUT); pinMode(LCD_D3, OUTPUT);
+pinMode(LCD_D4, OUTPUT); pinMode(LCD_D5, OUTPUT);
+pinMode(LCD_D6, OUTPUT); pinMode(LCD_D7, OUTPUT);
+*/
+
+
+/*
+// Icons als Byte-Array 
+*/
 
 // 'battery-charging', 24x24px
 const unsigned char epd_bitmap_battery_charging [] PROGMEM = {
@@ -70,19 +129,13 @@ const unsigned char epd_bitmap_heart [] PROGMEM = {
 };
 
 
-const int LCD_D0 = 13; const int LCD_D1 = 12;
-const int LCD_D2 = 33; const int LCD_D3 = 32;
-const int LCD_D4 = 25; const int LCD_D5 = 23;
-const int LCD_D6 = 22; const int LCD_D7 = 21;
-const int LCD_WR = 14; const int LCD_CS = 27;
-const int LCD_RS = 26; const int LCD_RST = 18;
-
-// Puffer: 1 Byte pro Pixel (Werte 0-31)
-// 240 * 160 = 38400 Bytes
-uint8_t framebuffer[38400]; 
-
-// Ein Byte-Wert wird in 8 Bit zerlegt und gleichzeitig auf den Datenbus geschrieben
+/*
+// Ein Byte-Wert wird in 8 Bit zerlegt und gleichzeitig auf den Datenbus geschrieben --------------
+*/
 void writeBus(uint8_t val) {
+  
+  // Legacy: Parallele Pins
+  /*
   digitalWrite(LCD_D0, (val & 0b00000001) >> 0 );
   digitalWrite(LCD_D1, (val & 0b00000010) >> 1 );
   digitalWrite(LCD_D2, (val & 0b00000100) >> 2 );
@@ -91,61 +144,53 @@ void writeBus(uint8_t val) {
   digitalWrite(LCD_D5, (val & 0b00100000) >> 5 );
   digitalWrite(LCD_D6, (val & 0b01000000) >> 6 );
   digitalWrite(LCD_D7, (val & 0b10000000) >> 7 );
+  */
+
+  digitalWrite(PIN_STORE, LOW);
+  shiftOut(PIN_DATA, PIN_SHIFT, MSBFIRST, val);
+  digitalWrite(PIN_STORE, HIGH);
   
   digitalWrite(LCD_WR, LOW); // Write enable clock input pin: The data on DB0 to DB15 are latched at the rising edge of the /WR signal. [14]
   delayMicroseconds(1);      // so wird eine steigende Flanke garantiert (1 us)
   digitalWrite(LCD_WR, HIGH);
 }
 
+/*
+// Sende einen BEFEHL an den Display-IC -----------------------------------------------------------
+*/
 void writeCmd(uint8_t cmd) {
-  digitalWrite(LCD_CS, LOW); // Chip enable - active LOW wenn Befehle gesendet werden sollen [14]
+  //digitalWrite(LCD_CS, LOW); // Chip enable - active LOW wenn Befehle gesendet werden sollen [14]
   digitalWrite(LCD_RS, LOW); // Wird ein Befehl geschrieben, ist RS (im Datenblatt A0) LOW [14]
   writeBus(cmd);             // das Byte wird parallel abgeschickt
-  digitalWrite(LCD_CS, HIGH);
+  //digitalWrite(LCD_CS, HIGH);
 }
 
+/*
+// Sende einen DATENSATZ an den Display-IC --------------------------------------------------------
+*/
 void writeData(uint8_t data) {
-  digitalWrite(LCD_CS, LOW); 
+  //digitalWrite(LCD_CS, LOW); 
   digitalWrite(LCD_RS, HIGH); // Werden Daten geschrieben, ist RS (im Datenblatt A0) HIGH [14]
   writeBus(data);             // das Byte wird parallel abgeschickt
-  digitalWrite(LCD_CS, HIGH);
+  //digitalWrite(LCD_CS, HIGH);
 }
 
-// --- 3. INIT (Bekannte Sequenz) ---
-void initDisplay() {
-    pinMode(LCD_D0, OUTPUT); pinMode(LCD_D1, OUTPUT);
-    pinMode(LCD_D2, OUTPUT); pinMode(LCD_D3, OUTPUT);
-    pinMode(LCD_D4, OUTPUT); pinMode(LCD_D5, OUTPUT);
-    pinMode(LCD_D6, OUTPUT); pinMode(LCD_D7, OUTPUT);
-    pinMode(LCD_WR, OUTPUT); pinMode(LCD_CS, OUTPUT);
-    pinMode(LCD_RS, OUTPUT); pinMode(LCD_RST, OUTPUT);
-    digitalWrite(LCD_CS, HIGH); // Chip enable active LOW
-    digitalWrite(LCD_WR, HIGH); // Write enable input,active LOW
 
-    digitalWrite(LCD_RST, LOW); delay(100); // Reset durchführen
-    digitalWrite(LCD_RST, HIGH); delay(100);
+/*
+//Initialisierungsprozess -------------------------------------------------------------------------
+*/
+void initDisplay() {
 
     writeCmd(0x30); // EXT IN (also EXT=0)
     writeCmd(0x94); // Sleep Out (Befehl 8)
-    writeCmd(0xd1); // Internal OSC on (Befehl 20)
-    
-    // ------------ Frickelige Stelle! -------------
 
-    writeCmd(0x20); // Power Control 
-    writeData(0b00001000); 
-    // Es steht nirgends explizit in der Befehlstabelle, dass man zuerst nur den Booster (0x08) und erst nach einer Pause den Rest (0x0B) einschalten darf.
-    // Das findet man nur ganz hinten in einem Beispiel-Flowchart auf S. 64 ("Power ON Sequence").
-    delay(10);
-    
     writeCmd(0x20);  // Power Control 
     writeData(0x0b); // Booster
     delay(10);
-
-    // --------------------------------------------
     
     // Duty 160
     writeCmd(0xca); // Display Control (Befehl 6)
-    writeData(0x04); 
+    writeData(0x00); 
     writeData(0x27); 
     writeData(0x00);
 
@@ -161,21 +206,20 @@ void initDisplay() {
     // Analog Tuning (Midas Spezifisch)
     writeCmd(0x31); // erweiterter Befehlssatz (EXT = 1)
     writeCmd(0x32); // Analog Circuit Set [37]
-    writeData(0x05); // Schöner Mist! Hier kann man raten, 
-    writeData(0x00); // was Midas gerne hätte. Das Midas-
-    writeData(0x02); // Datenblatt verschweigt das!
+    writeData(0x05); 
+    writeData(0x00);
+    writeData(0x02); 
 
     writeCmd(0x30); // zurück zum normalen Befehlssatz
     writeCmd(0xaf); // ON (Befehl 1)
 
     // Kontrast
-    writeCmd(0x81); // Increment Electronic Volume (Befehlt 24) DOPPELBYTE-BEFEEHL!
+    writeCmd(0x81); // VOLCTRL (Befehl 23) DOPPELBYTE-BEFEEHL!
     writeData(0b101101); // LSBs 101101 (D0-D5)
     writeData(0b011); // MSBs 011
     // -> 237 / 511
 
     // wird verwendet statt Befehl 23 ("VOLCTRL")
-    // warum auch immer????
     for(int i=0; i<34; i++) { 
       writeCmd(0xD6); // Befehl 24 "VOLUP" (EC Increase 1)
       delay(2); 
@@ -186,7 +230,9 @@ void initDisplay() {
     updateDisplay();
 }
 
-// Setzt Pixel im RAM. greyValue: 0 (Weiß) bis 31 (Schwarz)
+/*
+// Setzten eines Pixels in den Framebuffer --------------------------------------------------------
+*/
 void setPixel(int x, int y, uint8_t greyValue) {
     if (x < 0 || x >= 240 || y < 0 || y >= 160) return;
     
@@ -196,7 +242,7 @@ void setPixel(int x, int y, uint8_t greyValue) {
     framebuffer[y * 240 + x] = greyValue;
 }
 
-// Schickt RAM an Display und wandelt Formate um
+// Schickt RAM an Display und wandelt Formate um --------------------------------------------------
 void updateDisplay() {
     writeCmd(0b00010101); // Bereich Spalten (Befehl 10; "Column Address Set") [41]
     writeData(0);   // von 0
@@ -206,41 +252,23 @@ void updateDisplay() {
     writeData(0); // von 0
     writeData(0x9F); // bis 160 (Zeilen sind nicht zusammengefasst)
 
-
-    // Das Display geht automatisch zum nächsten Pixel über, wenn es erkannthat, dass ein güktiger Pixel gesendet wurde!
+    // Das Display geht automatisch zum nächsten Pixel über, wenn es erkannt hat, dass ein gültiger Pixel gesendet wurde!
     // "the line address is incremented by 1 and the column address is returned to the start column" [44]
     // -> es wird Spaltenweise inkrementiert und beim Überlauf in die nächste Zeile gewechselt
-    
     
     writeCmd(0b01011100); // RAM Write Start (Befehl 12: "Memory write") [36]
     // "the write mode is automatically cancelled if any other command is entered." [44]
 
-    // Hier werdne ide Daten aus dem Framebuffer 3-Pixel-weise auf den Datenbus geschickt
+    // Hier werdne die Daten aus dem Framebuffer 3-Pixel-weise auf den Datenbus geschickt
     for (long i = 0; i < 38400; i += 3) {
         // Hole 3 Grauwerte (0-31)
         uint8_t p1 = framebuffer[i];
         uint8_t p2 = framebuffer[i+1];
         uint8_t p3 = framebuffer[i+2];
 
-
         // Ein Pixel hat 32 Graustufen -> 5 Bit
         // ein Doppelbyte hat 16 Bit
         // gemäß 2B3P, 8-bit mode [22]
-
-        // Der Datenstrom besteht aus:
-        // Byte 1: P1 [1.7-1.3] P2 [1.2-1.0]
-        // Byte 2: P2 [2.7-2.6] P3 [2.4-1.0]
-
-        // oder anders: P1 (Bits 15-11) | P2 (Bits 10-6) | Unused (Bit 5) | P3 (Bits 4-0)
-
-        // [2.5] ist ein Don't Care
-        
-        // p1 hat den Wertebreich 0b00000-0b11111 und die hintersten 5 Bit im 2-Byte-Protkoll -> 16 - 5 = 11 ist absolute Wertigkeit des P1-LSB
-        // p2 hat den Wertebreich 0b00000-0b11111 und die Bits 6-11 im 2-Byte-Protkoll
-        // p3 hat den Wertebreich 0b00000-0b11111 und die Bits 0-5 im 2-Byte-Protkoll
-
-        //             -> [5 Bit P1] + [5 Bit P2] + 0 + [5 Bit P3]  (MSB-LSB; man stelle sich hier die Bits als String vor, keine Stellweise Addition!!)
-
         uint16_t data = (p1 << 11)   | (p2 << 6)       | (p3);
 
         // Senden (High Byte zuerst)
@@ -249,57 +277,51 @@ void updateDisplay() {
     }
 }
 
-// eigene Klasse, die von Adafruit_GFX erbt
+// eigene Klasse, die von Adafruit_GFX erbt -------------------------------------------------------
 class MyCustomDisplay : public Adafruit_GFX {
   public:
-    // Konstruktor: Übergib die Breite und Höhe der Matrix
     MyCustomDisplay(int16_t w, int16_t h) : Adafruit_GFX(w, h) {}
-
-    // Das ist die einzige Funktion, die zwingend implementiert werden muss
     void drawPixel(int16_t x, int16_t y, uint16_t color) override {
       setPixel(x, y, 0); 
     }
 };
 
-
-
+// Initialisierung mit Display-Größe (Variablenübergabe an Adafruit_GFX)
 MyCustomDisplay matrix(240, 160);
 
-
+/*
+// kleine Funktionen, um Text zu zentrieren / links / rechts darzustellen -------------------------
+*/
 void drawTextCentered(String text, int16_t x, int16_t y) {
   int16_t x1, y1;
   uint16_t w, h;
-  
   matrix.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
-  
   // Berechnung der X-Position
   x = (x - w/2);
-  
   // Wichtig: x1 korrigiert kleine Abweichungen im Font-Design
   matrix.setCursor(x - x1, y); 
   matrix.print(text);
 }
-
-
 void drawTextRight(String text, int16_t x, int16_t y) {
   int16_t x1, y1;
   uint16_t w, h;
-  
   matrix.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
-  
   x = x - w;
-  
   matrix.setCursor(x - x1, y);
   matrix.print(text);
 }
-
 void drawTextLeft(String text, int16_t x, int16_t y) {
   matrix.setCursor(x, y);
   matrix.print(text);
 }
 
 
-
+/*
+// Aufgepasst lieber Studienarbeits-Nachfolger!  --------------------------------------------------
+//Hier habe ich Dir eine Beispielfunktion gebastelt, welche 
+//über die Parameter der Funktion schon eine 
+//Anzeige darstellt (gemäß Speedcoach 2)
+*/
 void displayParameters(String spm, String split, String chrono, String distance) {
 
   // Zeichnen des Rasters
@@ -341,6 +363,7 @@ void displayParameters(String spm, String split, String chrono, String distance)
 
 }
 
+// Nebenfunktion, um die seit Systemstart verstrichene Zeit zu ermitteln --------------------------
 String getFormattedTime() {
   unsigned long currentMillis = millis();
 
@@ -360,23 +383,30 @@ String getFormattedTime() {
   return String(buffer);
 }
 
+void setup() { //------------------------------------------------------------------------------------
+  
+  pinMode(PIN_STORE, OUTPUT);
+  pinMode(PIN_SHIFT, OUTPUT);
+  pinMode(PIN_DATA, OUTPUT);
 
-void setup() {
+  pinMode(LCD_WR, OUTPUT);
+  pinMode(LCD_RS, OUTPUT);
+
+  digitalWrite(LCD_WR, HIGH); // Write enable input,active LOW
+
+
   initDisplay();
+  Serial.begin(115200);
 }
 
 void loop() {
 
-  // gesamtes Display leeren - hier muss in der Performance noch etwas nachgelegt werden - das ist ineffizient
-  for(int i=0; i<240; i++) {
-    for(int j=0; j<160; j++) {
-      setPixel(i, j, 31);
-    }
-  }
+  // gesamtes Display leeren - Wichtig, da Adafruit_GFX keine Pixel "löscht"!
+  memset(framebuffer, 31, sizeof(framebuffer));
 
   displayParameters("32","1:51",getFormattedTime(),"2056");
 
   updateDisplay();
 
-  delay(50);
+
 }
